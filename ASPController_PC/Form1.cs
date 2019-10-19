edbc53b280d3dda0a534a562ace4989b281c1268
nickload <nickload1613@gmail.com>
2019-10-19 22:13:16 +0700

    add client

---
+++ ASPController_PC/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+namespace ASPController_PC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string JSONData = await Task.Factory.StartNew(() => JsonConvert.SerializeObject(textBox1.Text));
+            WebRequest request = WebRequest.Create("https://localhost:44305/Home/Hello");
+
+            request.Method = "POST";
+
+            string quary = $"name={JSONData}";
+
+            byte[] byteMsg = Encoding.UTF8.GetBytes(quary);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            request.ContentLength = byteMsg.Length;
+
+            using (Stream stream = await request.GetRequestStreamAsync())
+            {
+                await stream.WriteAsync(byteMsg, 0, byteMsg.Length);
+            }
+
+            WebResponse response = await request.GetResponseAsync();
+
+            string answer = null;
+
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader sR = new StreamReader(s))
+                {
+                    answer = await sR.ReadToEndAsync();
+                }
+            }
+            response.Close();
+
+            string hellowStr = await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<string>(answer));
+
+            MessageBox.Show(hellowStr);
+        }
+    }
+}
---
+++ ASPController_PC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ASPController_PC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASPController_PC")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("035dc3d8-ea31-4749-9d7d-a714ddf5e5ac")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
